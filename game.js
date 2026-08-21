71f83ec75a4bb0cd85fbbb2386a10a9ee6553849
François Godin <francois.godin@mfa.fr>
2026-08-21 13:35:00 +0200

    ✨ draw grid and make it dynamic on resize

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 class Game {
   constructor(config) {
-    this.faceA           = config.faceA;
-    this.faceADim        = config.faceADim;
-    this.gameDim         = config.gameDim;
-    
-    this.tileW            = config.tileW;
-    this.tileH            = config.tileH;
-    this.displaytileScale = config.displaytileScale;
+    this.faceA          = config.faceA;
+    this.faceADim       = config.faceADim;
+    this.faceATileSize  = config.faceATileSize;
+
+    this.gridDim        = config.gridDim;
+    this.gridMaxSize    = config.gridMaxSize;
+    this.gridSize       = config.gridMaxSize;
+
+    this.gridPosition = createVector(config.gridX, config.gridY);
+
+    this.tileOffsetRatio = 0.05;
   
     this.tileset = [];
     this.init();
@@ -21,32 +25,56 @@ class Game {
       for (let y = 0; y < this.faceADim; y++) {
         const tile = new Tile(
           tileId++,
-          x * this.tileW,
-          y * this.tileH,
-          this.tileW,
-          this.tileH,
-          faceA
+          x * this.faceATileSize,
+          y * this.faceATileSize,
+          this.faceATileSize,
+          this.faceATileSize,
+          this.faceA
         );
         allTiles.push(tile);
       }
     }
 
-    const selected = shuffle(allTiles).slice(0, this.gameDim ** 2);
+    const selected = shuffle(allTiles).slice(0, this.gridDim ** 2);
+    this.tileset = selected;
+  }
 
-    const dw = this.tileW * this.displaytileScale;
-    const dh = this.tileH * this.displaytileScale;    
+  draw() {
+    for (const tile of this.tileset) {
+      tile.draw();
+    }
+  }
+
+  onResize() {
+    this.computeGridCoordinates();
+    this.computeTilesCoordinates();
+  }
+
+  computeGridCoordinates() {
+    // Calculer position et dimensions de la grille en fonction de width et height
+    const maxWidth = width * 0.8;
+    this.gridSize = Math.min(maxWidth, this.gridMaxSize)
+    this.gridPosition.x = width / 2 - this.gridSize / 2;
+  }
+
+  computeTilesCoordinates() {
+    // Calculer position et dimensions des cartes en fonction des position et dimensions de la grille
+    const totalOffsetRatio = this.tileOffsetRatio * (this.gridDim - 1);
+    const tileSize = this.gridSize / (this.gridDim + totalOffsetRatio);
+    const offset = tileSize * this.tileOffsetRatio;
+
     let index = 0;
-
-    for (let x = 0; x < this.gameDim; x++) {
-      for (let y = 0; y < this.gameDim; y++) {
-        const tile = selected[index++];
-        tile.dx = x * dw; 
-        tile.dy = y * dh;
-        tile.dw = dw;
-        tile.dh = dh;
+    for (let x = 0; x < this.gridDim; x++) {
+      for (let y = 0; y < this.gridDim; y++) {
+        const tile    = this.tileset[index++];
+        const offsetX = x > 0 ? offset : 0;
+        const offsetY = y > 0 ? offset : 0;
+        
+        tile.dx = this.gridPosition.x + x * (tileSize + offsetX);
+        tile.dy = this.gridPosition.y + y * (tileSize + offsetY);
+        tile.dw = tileSize;
+        tile.dh = tileSize;
       }
     }
-
-    this.tileset = selected;
   }
 }
--- sketch.js
+++ sketch.js
@@ -3,31 +3,37 @@ let game;
 
 const CONFIG = {
   FACEA_DIM: 5,
-  GAME_DIM: 4,
-  TILE_SIZE: 300,
-  DISPLAY_TILE_SCALE: 0.5
+  FACEA_TILE_SIZE: 300,
+  GRID_DIM: 4,
+  GRID_X: 100,
+  GRID_Y: 100,
+  GRID_MAX_SIZE: 600
 }
 
 async function setup() {
   backgroundImg = await loadImage('/assets/background.png');
   faceA         = await loadImage('/assets/faceA.jpg'); // 1500x1500 (5x5 tiles)
-  
+
   game = new Game({
-    faceA           : faceA,
-    faceADim        : CONFIG.FACEA_DIM,
-    gameDim         : CONFIG.GAME_DIM,
-    tileW           : CONFIG.TILE_SIZE,
-    tileH           : CONFIG.TILE_SIZE,
-    displaytileScale: CONFIG.DISPLAY_TILE_SCALE
+    faceA        : faceA,
+    faceADim     : CONFIG.FACEA_DIM,
+    gridDim      : CONFIG.GRID_DIM,
+    faceATileSize: CONFIG.FACEA_TILE_SIZE,
+    gridX        : CONFIG.GRID_X,
+    gridY        : CONFIG.GRID_Y,
+    gridMaxSize  : CONFIG.GRID_MAX_SIZE,
   })
 
   createCanvas(windowWidth, windowHeight);
+  game.onResize();
 }
 
 function draw() {
   image(backgroundImg, 0, 0, width, height, 0, 0, backgroundImg.width, backgroundImg.height, COVER);
+  game.draw();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  game.onResize();
 }
